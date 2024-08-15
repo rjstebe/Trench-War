b9268f8be215673f13b231f5020182446097dcd1
Ryan Stebe <rjstebe1@gmail.com>
2024-08-14 20:07:05 -0400

    Refactored order assignment to be based on priorities for future proofing

--- scripts/orders/order.gd
+++ scripts/orders/order.gd
@@ -10,3 +10,9 @@ func _remove_order():
 		rally_point._remove_rally_point()
 	order_removed.emit(self)
 	queue_free()
+
+func get_total_assigned_soldiers():
+	var total = 0
+	for rally_point in rally_points:
+		total += rally_point.assigned_soldiers
+	return total
--- scripts/player_manager.gd
+++ scripts/player_manager.gd
@@ -10,11 +10,25 @@ var clearing_order_scene = preload("res://scenes/clearing_order.tscn")
 enum Side {PLAYER, ENEMY}
 @export var side = Side.PLAYER
 
-# Each dictionary is a set for which build orders have which numbers of soldiers assigned to them
-var build_order_rally_points:Array[Dictionary] = [{},{},{}]
+# Priority of orders:
+# Manual orders (soldiers are never taken automatically)
+# Minimum clearing and supporting logistics (up to 1 soldier per path)
+# Expected required clearing and supporting logistics (1.5x equal soldiers to enemy rounded up to a maximum of 3? soldiers per path)
+# Man defensive emplacements and supporting logistics (Machine gun, AA gun, etc)
+# Defend trench and supporting logisitics (up to 2 soldiers per trench that can "see" enemy trenches or the back edge)
+# Man other emplacements and supporting logistics
+# Fill supply buffers (sorted by priority then distance)
+# Line up/charge orders and logistics
+# Construction orders and logistics
+# Recover morale
+# Idle
+
+var rally_points_by_assignment_priority = []
+var rally_point_priority_comparator = func(a,b): return _get_rally_point_assignment_priority(a) > _get_rally_point_assignment_priority(b)
+
 var build_trench_order_location_lookup:Dictionary = {}
-var clearing_order_rally_points:Array[Dictionary] = [{},{},{}]
 var clearing_order_location_lookup:Dictionary = {}
+
 var _update_assignments_this_frame:bool = true
 
 func _physics_process(_delta):
@@ -33,14 +47,7 @@ func _on_clearing_order_removed(clearing_order:ClearingOrder):
 	clearing_order_location_lookup.erase([clearing_order.hex_position, clearing_order.target_direction])
 
 func _on_rally_point_removed(rally_point:RallyPoint):
-	#Remove rally point from build order rally points if applicable
-	if rally_point.get_parent() is BuildOrder:
-		for i in range(0,2):
-			build_order_rally_points[i].erase(rally_point)
-	#Clear rally point from clearing order rally points if applicable
-	if rally_point.get_parent() is ClearingOrder:
-		for i in range(0,2):
-			clearing_order_rally_points[i].erase(rally_point)
+	rally_points_by_assignment_priority.erase(rally_point)
 	_update_assignments_this_frame = true
 
 func _on_trench_vision_updated():
@@ -75,7 +82,7 @@ func create_build_trench_order(trench_position:Array):
 	build_trench_order.order_removed.connect(_on_build_order_removed)
 	for rally_point in build_trench_order.rally_points:
 		rally_point.rally_point_removed.connect(_on_rally_point_removed)
-		build_order_rally_points[0][rally_point] = null
+		_insert_rally_point_by_priority(rally_point)
 	build_trench_order_location_lookup[[trench_position[0], trench_position[1]]] = build_trench_order
 	build_trench_order_location_lookup[[trench_position[1], trench_position[0]]] = build_trench_order
 	_update_assignments_this_frame = true
@@ -89,83 +96,84 @@ func create_clearing_trench_order(trench_position:Array):
 	add_child(clearing_order)
 	clearing_order.order_removed.connect(_on_clearing_order_removed)
 	clearing_order.rally_points[0].rally_point_removed.connect(_on_rally_point_removed)
-	clearing_order_rally_points[0][clearing_order.rally_points[0]] = null
+	_insert_rally_point_by_priority(clearing_order.rally_points[0])
 	clearing_order_location_lookup[trench_position] = clearing_order
 	_update_assignments_this_frame = true
 
-func get_build_trench_order_by_location(hex_position_a:Vector2i, hex_position_b:Vector2i):
-	if build_trench_order_location_lookup.has([hex_position_a, hex_position_b]):
-		return build_trench_order_location_lookup[[hex_position_a, hex_position_b]]
+#func get_build_trench_order_by_location(hex_position_a:Vector2i, hex_position_b:Vector2i):
+	#if build_trench_order_location_lookup.has([hex_position_a, hex_position_b]):
+		#return build_trench_order_location_lookup[[hex_position_a, hex_position_b]]
 
-#TODO: order priority system based on type of order, and how many soldiers are assigned
-# e.g. the second soldier of an order is lower priority than the first,
-# clearing orders are higher than most other orders but prioritizes pulling soldiers
-# from lower priority orders/soldiers
-# TODO: All soldiers on a clearing order should have their order assignment cleared when order assignments are changed
 func update_order_assignments():
-	# Assign minimum soldiers to trench clearing orders
-	for i in range(0,1):
-		for rally_point in clearing_order_rally_points[i].keys():
-			var soldier_hex = building_grid.trench_pathfinding.get_closest_hex_by_trench( \
-				building_grid.local_to_map(rally_point.position), \
-				func lambda(hex):
-					if soldier_map.hex_exists(hex):
-						for soldier in soldier_map.trench_occupation[side][hex]:
-							if _clearing_soldier_selection_condition(soldier):
-								return true
-					return false,
-				func lambda(hex):
-					for given_side in Side.values():
-						if given_side != side and soldier_map.soldier_vision_counts[given_side][hex] != 0:
-							return hex != building_grid.local_to_map(rally_point.position)
-					return false
-			)
+	rally_points_by_assignment_priority.sort_custom(rally_point_priority_comparator)
+	for i in range(0, rally_points_by_assignment_priority.size()):
+		var rally_point = rally_points_by_assignment_priority[i]
+		var rally_point_priority = _get_rally_point_assignment_priority(rally_point)
+		if rally_point_priority <= 0:
+			break
+		var soldier_hex = building_grid.trench_pathfinding.get_closest_hex_by_trench( \
+		building_grid.local_to_map(rally_point.position), \
+		func lambda(hex):
+			if soldier_map.hex_exists(hex):
+				for potential_soldier in soldier_map.trench_occupation[side][hex]:
+					if _get_soldier_persistance_priority(potential_soldier) < rally_point_priority:
+						return true
+			return false,
+		func lambda(hex):
+			for given_side in Side.values():
+				if given_side != side and soldier_map.soldier_vision_counts[given_side][hex] != 0:
+					return hex != building_grid.local_to_map(rally_point.position)
+			return false
+		)
+		if soldier_hex != null:
 			var soldier = null
-			if soldier_hex != null:
-				for potential_soldier in soldier_map.trench_occupation[side][soldier_hex]:
-					if _clearing_soldier_selection_condition(potential_soldier):
-						soldier = potential_soldier
-						break
-				# Update prior rally point data if applicable
-				var prior_rally_point = soldier.current_rally_point
-				if prior_rally_point != null and prior_rally_point.get_parent() is BuildOrder:
-					var assigned_soldier_count = prior_rally_point.assigned_soldiers.size()
-					build_order_rally_points[assigned_soldier_count].erase(prior_rally_point)
-					build_order_rally_points[assigned_soldier_count-1][prior_rally_point] = null
-				# Assign soldier to new rally point
-				soldier.set_rally_point(rally_point)
-				clearing_order_rally_points[i].erase(rally_point)
-				clearing_order_rally_points[i+1][rally_point] = null
-	# Assign remaining idle soldiers to build orders
-	for i in range(0,2):
-		for rally_point in build_order_rally_points[i].keys():
-			var soldier_hex = building_grid.trench_pathfinding.get_closest_hex_by_trench( \
-				building_grid.local_to_map(rally_point.position), \
-				func lambda(hex):
-					if soldier_map.hex_exists(hex):
-						for soldier in soldier_map.trench_occupation[side][hex]:
-							if _building_soldier_selection_condition(soldier):
-								return true
-					return false,
-				func lambda(hex):
-					for given_side in Side.values():
-						if given_side != side and soldier_map.soldier_vision_counts[given_side][hex] != 0:
-							return true
-					return false
-			)
-			var soldier = null
-			if soldier_hex != null:
-				for potential_soldier in soldier_map.trench_occupation[side][soldier_hex]:
-					if _building_soldier_selection_condition(potential_soldier):
-						soldier = potential_soldier
-						break
-				# Assign soldier to new rally point
-				soldier.set_rally_point(rally_point)
-				build_order_rally_points[i].erase(rally_point)
-				build_order_rally_points[i+1][rally_point] = null
+			for potential_soldier in soldier_map.trench_occupation[side][soldier_hex]:
+				if _get_soldier_persistance_priority(potential_soldier) < rally_point_priority:
+					soldier = potential_soldier
+			var prior_rally_point = soldier.current_rally_point
+			soldier.set_rally_point(rally_points_by_assignment_priority[i])
+			if prior_rally_point != null:
+				_update_rally_point_priority(prior_rally_point)
+			_update_rally_point_priority(rally_point)
 
-func _clearing_soldier_selection_condition(_soldier):
-	return true
+# update's rally point's priority and moves it's position in list accordingly
+func _update_rally_point_priority(rally_point:RallyPoint):
+	var i = rally_points_by_assignment_priority.find(rally_point)
+	var done = false
+	while i - 1 >= 0 and \
+	_get_rally_point_assignment_priority(rally_points_by_assignment_priority[i-1]) < _get_rally_point_assignment_priority(rally_point):
+		rally_points_by_assignment_priority[i] = rally_points_by_assignment_priority[i - 1]
+		rally_points_by_assignment_priority[i - 1] = rally_point
+		i -= 1
+		done = true
+	if not done:
+		while i + 1 <= rally_points_by_assignment_priority.size() - 1 and \
+		_get_rally_point_assignment_priority(rally_points_by_assignment_priority[i+1]) > _get_rally_point_assignment_priority(rally_point):
+			rally_points_by_assignment_priority[i] = rally_points_by_assignment_priority[i + 1]
+			rally_points_by_assignment_priority[i + 1] = rally_point
+			i += 1
 
-func _building_soldier_selection_condition(soldier):
-	return soldier.current_rally_point == null
+func _insert_rally_point_by_priority(rally_point:RallyPoint):
+	var i = rally_points_by_assignment_priority.bsearch_custom(rally_point, rally_point_priority_comparator, false)
+	rally_points_by_assignment_priority.insert(i, rally_point)
+
+func _get_rally_point_assignment_priority(rally_point):
+	return _get_rally_point_assignment_priority_with_count(rally_point, rally_point.assigned_soldiers.size())
+
+func _get_rally_point_assignment_priority_with_count(rally_point, assigned_soldier_count):
+	if rally_point.get_parent() is ClearingOrder:
+		if assigned_soldier_count >= 1:
+			return 0
+		return 10 - 0.1*assigned_soldier_count
+	if rally_point.get_parent() is BuildOrder:
+		if assigned_soldier_count >= 2:
+			return 0
+		return 1 - 0.1*assigned_soldier_count
+	return 0
+
+# Should always be equal to or higher than assignment priority for the same scenario
+func _get_soldier_persistance_priority(soldier:Soldier):
+	var rally_point = soldier.current_rally_point
+	if rally_point == null:
+		return 0
+	return _get_rally_point_assignment_priority_with_count(rally_point, rally_point.assigned_soldiers.size()-1)
